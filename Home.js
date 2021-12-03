5293e06b8f1ca836e68ed9e3e7d908501aecfbfa
ChandraVamsiM <84317574+ChandraVamsiM@users.noreply.github.com>
2021-12-03 17:04:30 +0530

    Add files via upload

---
+++ Home.js
@@ -0,0 +1,12 @@
+import React,{Component} from 'react';
+
+export class Home extends Component{
+    render(){
+        return(
+            <div>
+                <h3>Welcome to Employee Registration Form</h3>
+                <h5>Fill All the Details of Employee in Employee Section</h5>
+            </div>
+        )
+    }
+}
---
+++ Variable.js
@@ -0,0 +1,4 @@
+export const variables={
+    API_URL:"http://localhost:46209/api/",
+    Photo_URL:"http://localhost:46209/Photos/"
+}
